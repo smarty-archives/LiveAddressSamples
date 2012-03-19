cb7995d5ae316b6f3085e17886ba7bdee0127d66
Whatcott, Michael <mike@smartystreets.com>
2012-03-19 16:38:12 -0600

    Updated samples with different address.

---
+++ python/street-address-batch.py
@@ -0,0 +1,33 @@
+import json
+import pprint
+import urllib
+import urllib2
+
+URL = 'http://api.qualifiedaddress.dev/street-address/?auth-token={0}'.format(
+    urllib.quote(r'YOUR_AUTHENTICATION_TOKEN_HERE')
+)
+
+addresses = [
+    {
+        'street': '1 infinite loop',
+        'city': 'cupertino',
+        'state': 'ca',
+        'zipCode': '95014',
+        'candidates': '10',
+    },
+    {
+        'street': '1600 Pennsylvania ave',
+        'city': 'Washington',
+        'state': 'DC',
+        'zipCode': '20500',
+        'candidates': '10',
+    }
+]
+
+DATA = json.dumps(addresses)
+
+request = urllib2.Request(URL, data=DATA)
+
+response = urllib2.urlopen(request)
+
+print pprint.pformat(json.loads(response.read()))
