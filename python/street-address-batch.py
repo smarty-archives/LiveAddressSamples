e24ac99ff97e34161609d03d92eaaabacfbfe380
Whatcott, Michael <mike@smartystreets.com>
2012-03-26 14:54:42 -0600

    Using correct url and protocol.

--- python/street-address-batch.py
+++ python/street-address-batch.py
@@ -3,8 +3,8 @@ import pprint
 import urllib
 import urllib2
 
-URL = 'http://api.qualifiedaddress.dev/street-address/?auth-token={0}'.format(
-    urllib.quote(r'YOUR_AUTHENTICATION_TOKEN_HERE')
+URL = 'https://api.qualifiedaddress.com/street-address/?auth-token={0}'.format(
+    urllib.quote(r'YOUR_AUTHENTICATION_TOKEN')
 )
 
 addresses = [
@@ -27,7 +27,6 @@ addresses = [
 DATA = json.dumps(addresses)
 
 request = urllib2.Request(URL, data=DATA)
-
 response = urllib2.urlopen(request)
 
 print pprint.pformat(json.loads(response.read()))
