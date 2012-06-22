cbba29d420c9f393f13adaa201272d03ffa3f298
Matthew Holt <Matthew.Holt+git@gmail.com>
2012-06-22 09:57:15 -0600

    'components' now includes the last_line (for convenience). Also if a street line 2 was returned, it is combined into first_line, for convenience and also validity.

--- javascript/liveaddress.js
+++ javascript/liveaddress.js
@@ -226,9 +226,9 @@ var LiveAddress = (function()
 				var comp = [];
 				for (idx in data)
 				{
-					data[idx].components.street_line1 = data[idx].delivery_line_1;
+					data[idx].components.first_line = data[idx].delivery_line_1;
 					if (typeof data[idx].delivery_line_2 !== "undefined")
-						data[idx].components.street_line1 += " " + data[idx].delivery_line_2;
+						data[idx].components.first_line += " " + data[idx].delivery_line_2;
 					data[idx].components.last_line = data[idx].last_line;
 					comp.push(data[idx].components);
 				}
