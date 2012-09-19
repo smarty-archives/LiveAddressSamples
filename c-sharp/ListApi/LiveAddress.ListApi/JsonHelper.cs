6728b8263e43744c96d3761cb17d835df071a0f0
Whatcott, Michael <mike@smartystreets.com>
2012-09-19 14:49:34 -0600

    Included a set of C# List API examples.

---
+++ c-sharp/ListApi/LiveAddress.ListApi/JsonHelper.cs
@@ -0,0 +1,87 @@
+﻿namespace LiveAddress.ListApi
+{
+	using System;
+	using System.Collections.Generic;
+	using System.Linq;
+	using System.Text;
+
+	// This class from the accepted answer to this question: 
+	// http://stackoverflow.com/questions/4580397/json-formatter-in-c
+
+	public class JsonHelper
+	{
+		private const string IndentString = "    ";
+
+		public static void PrintJson(string str)
+		{
+			Console.WriteLine("\n" + FormatJson(str) + "\n");
+		}
+
+		static string FormatJson(string str)
+		{
+			var indent = 0;
+			var quoted = false;
+			var sb = new StringBuilder();
+			for (var i = 0; i < str.Length; i++)
+			{
+				var ch = str[i];
+				switch (ch)
+				{
+					case '{':
+					case '[':
+						sb.Append(ch);
+						if (!quoted)
+						{
+							sb.AppendLine();
+							Enumerable.Range(0, ++indent).ForEach(item => sb.Append(IndentString));
+						}
+						break;
+					case '}':
+					case ']':
+						if (!quoted)
+						{
+							sb.AppendLine();
+							Enumerable.Range(0, --indent).ForEach(item => sb.Append(IndentString));
+						}
+						sb.Append(ch);
+						break;
+					case '"':
+						sb.Append(ch);
+						var escaped = false;
+						var index = i;
+						while (index > 0 && str[--index] == '\\')
+							escaped = !escaped;
+						if (!escaped)
+							quoted = !quoted;
+						break;
+					case ',':
+						sb.Append(ch);
+						if (!quoted)
+						{
+							sb.AppendLine();
+							Enumerable.Range(0, indent).ForEach(item => sb.Append(IndentString));
+						}
+						break;
+					case ':':
+						sb.Append(ch);
+						if (!quoted)
+							sb.Append(" ");
+						break;
+					default:
+						sb.Append(ch);
+						break;
+				}
+			}
+			return sb.ToString();
+		}
+	}
+
+	static class Extensions
+	{
+		public static void ForEach<T>(this IEnumerable<T> ie, Action<T> action)
+		{
+			foreach (var i in ie)
+				action(i);
+		}
+	}
+}
---
+++ c-sharp/ListApi/LiveAddress.ListApi/Program.cs
@@ -0,0 +1,53 @@
+﻿namespace LiveAddress.ListApi
+{
+	using System;
+	using System.Configuration;
+	using System.Threading;
+	using System.Web;
+
+	public static class Program
+	{
+		private static readonly TimeSpan PollingFrequency = TimeSpan.FromSeconds(30);
+		private static readonly TimeSpan GiveShortListsAChanceToCompleteBeforeCheckingStatusForTheFirstTime = TimeSpan.FromSeconds(5);
+		private static readonly string AuthId = ConfigurationManager.AppSettings["AuthId"];
+		private static readonly string AuthToken = HttpUtility.UrlEncode(ConfigurationManager.AppSettings["AuthToken"]);
+		private static readonly string InputList = ConfigurationManager.AppSettings["InputList"];
+		private static readonly string OutputDestination = ConfigurationManager.AppSettings["OutputDestination"];
+		private static readonly ListApiWrapper ListApi = new ListApiWrapper(AuthId, AuthToken);
+
+		public static void Main()
+		{
+			ListApi.ShowAllLists();
+
+			var newListId = ListApi.UploadList(InputList);
+
+			WaitForListToComplete(newListId);
+
+			ListApi.ShowAllLists(); // should include the list you uploaded just now
+
+			ListApi.DownloadCompleteList(newListId, OutputDestination); // Deducts necessary credits from your account (will fail with HTTP 402 "Payment Required" if you don't have enough)
+
+			ListApi.DeleteList(newListId);
+
+			ListApi.ShowAllLists(); // should match the initial output
+
+			Console.WriteLine("<ENTER> to quit...");
+			Console.ReadLine();
+		}
+
+		private static void WaitForListToComplete(Guid newListId)
+		{
+			while (true)
+			{
+				Thread.Sleep(GiveShortListsAChanceToCompleteBeforeCheckingStatusForTheFirstTime);
+
+				var finished = ListApi.CheckStatusOfList(newListId);
+				if (finished)
+					break;
+
+				Console.WriteLine("List is still in process--will check status again in {0}", PollingFrequency);
+				Thread.Sleep(PollingFrequency);
+			}
+		}
+	}
+}
