56b9e910deb7c6e69f1cb9daa717c5b1c1a08a9f
Matthew Holt <Matthew.Holt+git@gmail.com>
2012-11-09 12:09:36 -0700

    New PHP examples for the ZIP Code API

---
+++ php/get_zipcode_api.php
@@ -0,0 +1,22 @@
+<?php
+
+// Customize this (get ID/token values in your SmartyStreets account)
+$authId = urlencode("raw ID here");
+$authToken = urlencode("raw token here");
+
+// Input. You can fill out any combination of the 3 values and leave any blank
+$city = urlencode("");
+$state = urlencode("");
+$zipcode = urlencode("90023");
+
+// Build the URL
+$req = "https://api.smartystreets.com/zipcode/?city={$city}&state={$state}&zipcode={$zipcode}&auth-id={$authId}&auth-token={$authToken}";
+
+// GET request and turn into associative array
+$result = json_decode(file_get_contents($req));
+
+echo "<pre>";
+print_r($result);
+echo "</pre>";
+
+?>
---
+++ php/post_zipcode_api.php
@@ -0,0 +1,61 @@
+<?php
+
+// Zip Code API example (provided as-is, for instruction only)
+// by SmartyStreets: smartystreets.com
+// POST example with PHP that doesn't use cURL.
+
+
+// Your authentication ID/token (obtained in your SmartyStreets account)
+$authId = urlencode("raw id here");
+$authToken = urlencode("raw token here");
+
+// Your input... remember that this endpoint can accept practically any combination
+// of zip code, city, and/or state -- give what you can, we'll give back what we can.
+// You can send up to 100 lookups per request
+$addresses = array(
+    array(
+        "city" => "Cupertino",
+        "state" => "CA",
+        "zipcode" => "95014"
+    ),
+    array(
+        "zipcode" => "20500"
+    ),
+    array(
+        "state" => "WY"
+    ),
+    array(
+        "city" => "lebanon",
+        "state" => "kansas"
+    )
+);
+
+
+// This endpoint expects JSON
+$post = json_encode($addresses);
+
+
+// Create the stream context (a context is like metadata)
+$context = stream_context_create(
+    array(
+        "http" => array(
+        "method" => "POST",
+        "header" => "Content-Type: application/x-www-form-urlencoded\r\n"
+                    ."Content-Length: ".strlen($post)."\r\n",  
+        "content" => $post
+        )
+    )
+);
+
+
+// Do the request
+$page = file_get_contents("https://api.smartystreets.com/zipcode?auth-id={$authId}&auth-token={$authToken}", false, $context);
+
+
+// Show results
+echo "<pre>";
+print_r(json_decode($page));
+echo "</pre>";
+
+
+?>
