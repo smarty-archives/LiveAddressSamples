63acb404fad0a15ea88fa152f38804ad67d45c96
Michael Whatcott <mdwhatcott@gmail.com>
2014-08-15 12:47:39 -0600

    Using double quotes to discourage invalid JSON.
    This isn't necessary for this script because the single quotes are defining python strings, which are then serialized to JSON by the json module.

--- python/street-address-batch.py
+++ python/street-address-batch.py
@@ -6,25 +6,25 @@ import urllib2
 # Obtain an authentication ID/token pair from your
 # SmartyStreets account and put them in below.
 
-URL = 'https://api.smartystreets.com/street-address/?auth-id={0}&auth-token={1}'.format(
-    urllib.quote(r'<auth id here>'),
-    urllib.quote(r'<raw auth token here>')
+URL = "https://api.smartystreets.com/street-address/?auth-id={0}&auth-token={1}".format(
+    urllib.quote(r"<auth id here>"),
+    urllib.quote(r"<raw auth token here>")
 )
 
 addresses = [
     {
-        'street': '1 infinite loop',
-        'city': 'cupertino',
-        'state': 'ca',
-        'zipCode': '95014',
-        'candidates': '10',
+        "street": "1 infinite loop",
+        "city": "cupertino",
+        "state": "ca",
+        "zipCode": "95014",
+        "candidates": "10",
     },
     {
-        'street': '1600 Pennsylvania ave',
-        'city': 'Washington',
-        'state': 'DC',
-        'zipCode': '20500',
-        'candidates': '10',
+        "street": "1600 Pennsylvania ave",
+        "city": "Washington",
+        "state": "DC",
+        "zipCode": "20500",
+        "candidates": "10",
     }
 ]
 
