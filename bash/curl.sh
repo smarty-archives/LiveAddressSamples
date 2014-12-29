768e0fd6045b866a8bb4dac5489f07078215a523
Michael Whatcott <mdwhatcott@gmail.com>
2014-12-29 10:12:48 -0700

    Removed old examples.

--- bash/curl.sh
+++
@@ -1,5 +0,0 @@
-# Output goes to results.json file:
-curl -o -v results.json "https://api.smartystreets.com/street-address?street=579+N+600+W&ssecondary=Apt+3city=Provo&state=UT&zipcode=84601&auth-id=<your-auth-id-here>&auth-token=<your-urlencoded-auth-token-here>"
-
-# Output to stdout:
-curl -v "https://api.smartystreets.com/street-address?street=579+N+600+W&ssecondary=Apt+3city=Provo&state=UT&zipcode=84601&auth-id=auth-id=<your-auth-id-here>&auth-token=<your-urlencoded-auth-token-here>"
